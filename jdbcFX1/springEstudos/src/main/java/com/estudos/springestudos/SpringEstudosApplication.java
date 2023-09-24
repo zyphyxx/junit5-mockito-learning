2470a2734c36631fa1943167d608ab9537a68ec6
Edgar Jesus <mendesbr92@gmail.com>
2023-09-23 22:56:44 -0300

    tentando conectar o mysql

---
+++ jdbcFX1/springEstudos/src/main/java/com/estudos/springestudos/SpringEstudosApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.springestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEstudosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEstudosApplication.class, args);
+    }
+
+}
---
+++ jdbcFX1/springEstudos/src/test/java/com/estudos/springestudos/SpringEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.springestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEstudosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
