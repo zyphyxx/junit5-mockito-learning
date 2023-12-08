4076ffe9dcde828d24c53bfb68166079272a78db
Edgar Jesus <mendesbr92@gmail.com>
2023-12-07 21:35:54 -0300

    first commit

---
+++ junit-mockito/src/main/java/com/zpx/junitmockito/JunitMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.zpx.junitmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitMockitoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunitMockitoApplication.class, args);
+    }
+
+}
