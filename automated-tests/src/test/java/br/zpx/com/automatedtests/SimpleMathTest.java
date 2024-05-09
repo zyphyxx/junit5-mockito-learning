079ef852ba87e567980301484d0f59c81154011f
Edgar Jesus <mendesbr92@gmail.com>
2024-05-09 20:13:17 -0300

    BDD Given, When, Then, Ciclo de vida com BeforeAll, AfterAll, BeforeEach, AfterEach

--- automated-tests/src/test/java/br/zpx/com/automatedtests/SimpleMathTest.java
+++ automated-tests/src/test/java/br/zpx/com/automatedtests/SimpleMathTest.java
@@ -1,59 +1,104 @@
 package br.zpx.com.automatedtests;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Teste de Operações da Classe Simple Math")
 class SimpleMathTest {
 
+    //test[System Under Test]_[Condition or State Change]_[Expected result]
+    @BeforeAll
+    static void setUp() {
+        System.out.println("Running @BeforeAll method!");
+    }
+
+    @AfterAll
+    static void cleanUp() {
+        System.out.println("Running @AfterAll method!");
+    }
+
+    SimpleMath math;
+
+    @BeforeEach
+    void beforeEachMethod() {
+        math = new SimpleMath();
+    }
+
+    @AfterEach
+    void afterEachMethod() {
+        System.out.println("Running @AfterEach method!");
+
+    }
+
     @Test
+    @DisplayName("Test 2 + 2 = 4")
     void sum() {
 
-        SimpleMath simpleMath = new SimpleMath();
+        // given
         Double firstNumber = 2D;
         Double secondNumber = 2D;
-
-        Double actual = simpleMath.sum(firstNumber, secondNumber);
         Double expected = 4D;
 
-        assertEquals(expected,actual,
-                () -> firstNumber +" + "+secondNumber+" Não retornou o resultado esperado!"); // lazy assert messages
-        assertNotEquals(5D,actual);
+        //when
+        Double actual = math.sum(firstNumber, secondNumber);
+
+        //then
+        assertEquals(expected, actual,
+                () -> String.format("A soma entre %.2f + %.2f , não retornou o valor %.2f", firstNumber, secondNumber, expected)); // lazy assert messages
+        assertNotEquals(5D, actual);
         assertNotNull(actual);
 
     }
 
     @Test
+    @DisplayName("Teste 10 - 2 = 8")
     void subtraction() {
 
-        SimpleMath simpleMath = new SimpleMath();
+        //given
         Double firstNumber = 10D;
         Double secondNumber = 2D;
-
-        Double actual = simpleMath.subtraction(firstNumber,secondNumber);
         Double expected = 8D;
 
+        //when
+        Double actual = math.subtraction(firstNumber, secondNumber);
+
+        //then
         assertNotNull(firstNumber);
         assertNotNull(secondNumber);
-
-        assertEquals(expected,actual, () -> String.format("A Subtração entre %.2f - %.2f não resultou o resultado esperado",firstNumber,secondNumber));
-        assertNotEquals(4,expected);
+        assertEquals(expected, actual, () -> String.format("A Subtração entre %.2f - %.2f não resultou o resultado esperado", firstNumber, secondNumber));
+        assertNotEquals(4, expected);
 
     }
 
     @Test
+    @DisplayName("Test 10 * 2 = 20")
     void multiplication() {
+
+        //Given
+        Double firstNumber = 10D;
+        Double secondNumber = 2D;
+        Double expected = 20D;
+
+        // when
+        Double actual = math.multiplication(firstNumber, secondNumber);
+
+        //then
+        assertEquals(expected, actual, () -> String.format("A multiplicação de %.2f * %.2f não resultou no valor %.2f", firstNumber, secondNumber, expected));
+        assertNotNull(firstNumber);
+        assertNotNull(secondNumber);
+        assertNotEquals(10, actual);
+
+
     }
 
     @Test
+    @Disabled("TODO: Nos precisamos trabalhar com a divisão")
     void division() {
     }
 
     @Test
-    void mean() {
-    }
-
-    @Test
+    @Disabled("TODO: Nos precisamos trabalhar com a raiz quadrada")
     void squareRoot() {
     }
 }
